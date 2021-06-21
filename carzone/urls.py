4ac7f37c959e68dc5cc2e92469e9a5460e9862d8
Sajid96 <sajidhussain98@gmail.com>
2021-06-21 10:15:38 +0530

    minor changes in urls

--- carzone/urls.py
+++ carzone/urls.py
@@ -20,9 +20,10 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pages.urls')),
-    path('cars/', include('cars.urls')),
-    path('accounts/', include('accounts.urls')),
+    path('',include('pages.urls')),
+    path('cars/',include('cars.urls')),
+    path('accounts/',include('accounts.urls')),
     path('socialaccounts/', include('allauth.urls')),
     path('contacts/', include('contacts.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
